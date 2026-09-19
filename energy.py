384b8805f2cf1da5faea3964242afc905bef92ec
Carlos Valera <2501087+cookyt@users.noreply.github.com>
2026-09-18 19:04:12 -0700

    Fix car-gallons conversion & add more conversions

--- energy.py
+++ energy.py
@@ -21,12 +21,23 @@ tonne_co2_per_kwh = 3.94e-4
 
 # https://afdc.energy.gov/fuels/properties?fuels=GS
 btu_per_gasoline_gallon = 114_102
-btu_per_kwh = 3.409
+# https://www.eia.gov/energyexplained/units-and-calculators/british-thermal-units.php
+btu_per_kwh = 3412
 kwh_per_gasoline_gallon = btu_per_gasoline_gallon / btu_per_kwh
 
-# https://www.energy.gov/cmei/vehicles/articles/fotw-1360-sept-16-2024-typical-ev-87-91-efficient-compared-30-conventional
-kwh_car_per_kwh = 0.3
-kwh_car_per_gasoline_gallon = kwh_per_gasoline_gallon * kwh_car_per_kwh
+# https://www.energy.gov/cmei/vehicles/articles/fotw-1360-sept-16-2024-typical-ev-87-91-efficient-compared-30-conventional
+kwhcar_per_kwh = 0.3
+kwhcar_per_gasoline_gallon = kwhcar_per_kwh * kwh_per_gasoline_gallon
+
+# https://www.eia.gov/tools/faqs/faq.php?id=97&t=3
+kwh_per_house_year = 10_791
+
+# https://www.autozone.com/diy/trustworthy-advice/what-is-considered-good-gas-mileage
+typical_car_miles_per_gallon = 30
+
+# https://en.wikipedia.org/wiki/Harvesting_lightning_energy
+joules_per_lightning_bolt = 5e9
+kwh_per_lightning_bolt = joules_per_lightning_bolt / joules_per_kwh
 
 ###########
 
@@ -35,11 +46,19 @@ navstokes_h100_seconds = navstokes_gpt5_flop / h100_typ_flop_per_second
 navstokes_h100_joules = h100_typ_watts * navstokes_h100_seconds
 
 navstokes_kwh = navstokes_h100_joules / joules_per_kwh
-navstokes_car_gasoline_gallons = navstokes_kwh / kwh_car_per_gasoline_gallon
 navstokes_tonne_co2 = navstokes_kwh * tonne_co2_per_kwh
+navstokes_house_years = navstokes_kwh / kwh_per_house_year
+navstokes_car_gasoline_gallons = navstokes_kwh / kwhcar_per_gasoline_gallon
+navstokes_car_miles = navstokes_car_gasoline_gallons * typical_car_miles_per_gallon
+navstokes_lightning_bolts = navstokes_kwh / kwh_per_lightning_bolt
+
+def fmt(v):
+  return f"{v:>12,.0f}"
 
 print("Estimated Energy Cost of Open AI's Navier-Stokes Paper")
-print(f"{navstokes_kwh:>12,.2f} KWh")
-print(f"{navstokes_car_gasoline_gallons:>12,.2f} Gallons of gasoline in an internal-combustion car")
-print(f"{navstokes_tonne_co2:>12,.2f} Tonnes of CO2")
-
+print(f"| {fmt(navstokes_kwh)} | KWh of Electricity |")
+print(f"| {fmt(navstokes_tonne_co2)} | Tonnes of CO2 |")
+print(f"| {fmt(navstokes_house_years)} | American homes powered for one year |")
+print(f"| {fmt(navstokes_car_gasoline_gallons)} | Gallons of gasoline in an internal-combustion car |")
+print(f"| {fmt(navstokes_car_miles)} | Miles driven in a mid-size Sedan |")
+print(f"| {fmt(navstokes_lightning_bolts)} | Lightning Bolts |")
